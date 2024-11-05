bdb25231232713ad3bb7d65113c9b5030979d4b6
soso707 <sophia.rodes@escola.pr.gov.br>
2024-11-05 16:19:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo')
+        const proximoPasso = 'passo-' + this.getAttrebute('data-proxima')   });
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+})
